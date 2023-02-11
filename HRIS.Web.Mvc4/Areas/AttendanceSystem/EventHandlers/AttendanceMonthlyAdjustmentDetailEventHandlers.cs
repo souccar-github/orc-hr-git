3d357c9d333064dc13ccae7155adcb6fe5778874
Ali Ali <ali_essa_ali@outlook.com>
2023-02-11 14:55:57 +0400

    sorting the monthly and daily details

--- HRIS.Web.Mvc4/Areas/AttendanceSystem/EventHandlers/AttendanceMonthlyAdjustmentDetailEventHandlers.cs
+++ HRIS.Web.Mvc4/Areas/AttendanceSystem/EventHandlers/AttendanceMonthlyAdjustmentDetailEventHandlers.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using HRIS.Domain.AttendanceSystem.Entities;
 using Project.Web.Mvc4.Models;
 using Project.Web.Mvc4.Models.GridModel;
 
@@ -16,6 +17,12 @@ namespace project
             model.ToolbarCommands.RemoveAt(0);
             model.Views[0].ViewHandler = "RemoveEditButtonViewHandler";
             model.ViewModelTypeFullName = typeof(AttendanceMonthlyAdjustmentDetailEventHandlers).FullName;
+        }
+        public override void AfterRead(RequestInformation requestInformation, DataSourceResult result, int pageSize = 10, int skip = 0)
+        {
+            var allBeforeFilter = ((IQueryable<AttendanceMonthlyAdjustmentDetail>)result.Data).ToList();
+            result.Data = allBeforeFilter.OrderByDescending(x => x.Date).Skip(skip).Take(pageSize).AsQueryable();
+            result.Total = allBeforeFilter.Count();
         }
     }
 }
